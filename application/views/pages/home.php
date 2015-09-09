5566730d7b6a72cb0890ac8fc75a9c5ccf788f09
Ryan Carter <carterrjj@outlook.com>
2015-09-09 23:59:37 +0100

    Added note buttons

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,6 +1,54 @@
-<?php echo "hello";
+<?php
+
+echo "hello";
 
 ?>
+
+<style>
+#feedback {
+	font-size: 1.4em;
+}
+
+#selectable .ui-selecting {
+	background: #FECA40;
+}
+
+#selectable .ui-selected {
+	background: #F39814;
+	color: white;
+}
+
+#selectable {
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+	width: 450px;
+}
+
+#selectable li {
+	margin: auto;
+	padding: 1px;
+	float: left;
+	width: 40%;
+	font-size: 2em;
+	text-align: center;
+	clear: both;
+}
+</style>
+
+
+			<a class="btn btn-primary">A</a>
+			<a class="btn btn-primary">A#/Bb</a>
+			<a class="btn btn-primary">B</a>
+			<a class="btn btn-primary">C</a>
+			<a class="btn btn-primary">C#/Db</a>
+			<a class="btn btn-primary">D</a>
+			<a class="btn btn-primary">D#/Eb</a>
+			<a class="btn btn-primary">E</a>
+			<a class="btn btn-primary">F</a>
+			<a class="btn btn-primary">F#/Gb</a>
+			<a class="btn btn-primary">G</a>
+			<a class="btn btn-primary">G#/Ab</a>
 
 
 <!-- Buttons
@@ -13,96 +61,94 @@
 		</div>
 	</div>
 
-<!-- Typography
+	<!-- Typography
       ================================================== -->
-<div class="bs-docs-section">
-	<!-- Blockquotes -->
+	<div class="bs-docs-section">
+		<!-- Blockquotes -->
 
-	<div class="row">
-		<div class="col-lg-12">
-			<h2 id="type-blockquotes">Blockquotes</h2>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-lg-6">
-			<div class="bs-component">
-				<blockquote>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-						posuere erat a ante.</p>
-					<small>Someone famous in <cite title="Source Title">Source Title</cite></small>
-				</blockquote>
+		<div class="row">
+			<div class="col-lg-12">
+				<h2 id="type-blockquotes">Blockquotes</h2>
 			</div>
 		</div>
-		<div class="col-lg-6">
-			<div class="bs-component">
-				<blockquote class="pull-right">
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-						posuere erat a ante.</p>
-					<small>Someone famous in <cite title="Source Title">Source Title</cite></small>
-				</blockquote>
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="bs-component">
+					<blockquote>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							Integer posuere erat a ante.</p>
+						<small>Someone famous in <cite title="Source Title">Source Title</cite></small>
+					</blockquote>
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="bs-component">
+					<blockquote class="pull-right">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							Integer posuere erat a ante.</p>
+						<small>Someone famous in <cite title="Source Title">Source Title</cite></small>
+					</blockquote>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
-<!-- Tables
+	<!-- Tables
       ================================================== -->
-<div class="bs-docs-section">
+	<div class="bs-docs-section">
 
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="page-header">
-				<h1 id="tables">Tables</h1>
-			</div>
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="page-header">
+					<h1 id="tables">Tables</h1>
+				</div>
 
-			<div class="bs-component">
-				<table class="table table-striped table-hover ">
-					<thead>
-						<tr>
-							<th>#</th>
-							<th>Column heading</th>
-							<th>Column heading</th>
-							<th>Column heading</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>1</td>
-							<td>Column content</td>
-							<td>Column content</td>
-							<td>Column content</td>
-						</tr>
-						<tr>
-							<td>2</td>
-							<td>Column content</td>
-							<td>Column content</td>
-							<td>Column content</td>
-						</tr>
-						
-					</tbody>
-				</table>
+				<div class="bs-component">
+					<table class="table table-striped table-hover ">
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Column heading</th>
+								<th>Column heading</th>
+								<th>Column heading</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>Column content</td>
+								<td>Column content</td>
+								<td>Column content</td>
+							</tr>
+							<tr>
+								<td>2</td>
+								<td>Column content</td>
+								<td>Column content</td>
+								<td>Column content</td>
+							</tr>
+
+						</tbody>
+					</table>
+				</div>
+				<!-- /example -->
 			</div>
-			<!-- /example -->
 		</div>
 	</div>
-</div>
 
-<!-- Forms
+	<!-- Forms
       ================================================== -->
-<div class="bs-docs-section">
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="page-header">
-				<h1 id="forms">Forms</h1>
+	<div class="bs-docs-section">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="page-header">
+					<h1 id="forms">Forms</h1>
+				</div>
 			</div>
 		</div>
-	</div>
 
-	<div class="row">
-		<div class="col-lg-6">
-			<div class="bs-component">
-				
+		<div class="row">
+			<div class="col-lg-6">
+				<div class="bs-component"></div>
 			</div>
 		</div>
 	</div>
-</div>
