727ee766e918777b2911d7da9a0955f1f27f0910
Ryan Carter <carterrjj@outlook.com>
2015-09-10 01:26:51 +0100

    Create all 12 keys arrays

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -5,7 +5,28 @@ echo "hello";
 
 <script type="text/javascript">
 <!--
+var A = ["null", "A", "B", "C#/Db", "D", "E", "F#/Gb", "G#/Ab", "A"];
+var D = ["null", "D", "E", "F#/Gb", "G", "A", "B", "C#/Db", "D"];
+var G = ["null", "G", "A", "B", "C", "D", "E", "F#/Gb", "G" ];
+var C = ["null", "C", "D", "E", "F", "G", "A", "B", "C" ];
+var F = ["null", "F", "G", "A", "A#/Bb", "C", "D", "E", "F" ];
+var Bb = ["null", "A#/Bb", "C", "D", "D#/Eb", "F", "G", "A", "A#/Bb" ];
+var Eb = ["null", "D#/Eb", "F", "G", "G#/Ab", "Bb", "C", "D", "D#/Eb" ];
+var Ab = ["null", "G#/Ab", "A#/Bb", "C", "C#/Db", "D#/Eb", "F", "G", "G#/Ab"  ];
+var Db = ["null", "C#/Db", "D#/Eb", "F", "F#/Gb", "G#/Ab", "A#/Bb", "C", "C#/Db" ];
+var Gb = ["null", "F#/Gb", "G#/Ab", "A#/Bb", "B", "C#/Db", "D#/Eb", "F", "F#/Gb" ];
+var B = ["null", "B", "C#/Db", "D#/Eb", "E", "F#/Gb", "G#/Ab", "A#/Bb", "B" ];
+var E = ["null", "E", "F#/Gb", "G#/Ab", "A", "B", "C#/Db", "D#/Eb", "E" ];
+
+var All  = [A, D, G, C, F, Bb, Eb, Ab, Db, Gb, B, E];
 $(document).ready(function() {
+	for(i = 0; i < All.length; i++){
+		console.log(i + "------------" + All[i][1]);
+		for(k = 0; k < All[i].length; k++){
+			console.log(k + ": " + All[i][k]);
+		}
+	}
+
 $('#div_answerBox').click(function(e) {
     var txt = $(e.target).text();
     console.log(txt);
