5086f5cec7ebb5f894f0c5de5ebae9db35d9d631
Ryan Carter <carterrjj@outlook.com>
2015-09-11 23:21:50 +0100

    Fixed formatting

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,40 +2,53 @@
 ?>
 
 <script type="text/javascript">
-	MIDI.loadPlugin({
-	    soundfontUrl: "assets/soundfont/",
-	    instrument: "acoustic_grand_piano",
-	    onprogress: function(state, progress) {
-	        console.log(state, progress);
-	    },
-	    onsuccess: function() {
+MIDI.loadPlugin({
+    soundfontUrl: "assets/soundfont/",
+    instrument: "acoustic_grand_piano",
+    onprogress: function(state, progress) {
+        console.log(state, progress);
+    },
+    onsuccess: function() {
 
-	    	 $("#img_spinner").hide();
-		        $("#div_app").show();
-	        var delay = 0; // play one note every quarter second
-	        var note = 50; // the MIDI note
-	        var velocity = 127; // how hard the note hits
-	        // play the note
-	        MIDI.setVolume(0, 127);
-	        MIDI.noteOn(0, note, velocity, delay);
-	        MIDI.noteOff(0, note, delay + 0.75);
-	    }
-	});
+        $("#img_spinner").hide();
+        $("#div_app").show();
+        var delay = 0; // play one note every quarter second
+        var note = 50; // the MIDI note
+        var velocity = 127; // how hard the note hits
+        // play the note
+        MIDI.setVolume(0, 127);
+        MIDI.noteOn(0, note, velocity, delay);
+        MIDI.noteOff(0, note, delay + 0.75);
+    }
+});
 <!--
 var A = ["57", "A", "B", "C#/Db", "D", "E", "F#/Gb", "G#/Ab", "A"];
 var D = ["62", "D", "E", "F#/Gb", "G", "A", "B", "C#/Db", "D"];
-var G = ["55", "G", "A", "B", "C", "D", "E", "F#/Gb", "G" ];
-var C = ["60", "C", "D", "E", "F", "G", "A", "B", "C" ];
-var F = ["65", "F", "G", "A", "A#/Bb", "C", "D", "E", "F" ];
-var Bb = ["58", "A#/Bb", "C", "D", "D#/Eb", "F", "G", "A", "A#/Bb" ];
-var Eb = ["63", "D#/Eb", "F", "G", "G#/Ab", "A#/Bb", "C", "D", "D#/Eb" ];
-var Ab = ["56", "G#/Ab", "A#/Bb", "C", "C#/Db", "D#/Eb", "F", "G", "G#/Ab"  ];
-var Db = ["61", "C#/Db", "D#/Eb", "F", "F#/Gb", "G#/Ab", "A#/Bb", "C", "C#/Db" ];
-var Gb = ["66", "F#/Gb", "G#/Ab", "A#/Bb", "B", "C#/Db", "D#/Eb", "F", "F#/Gb" ];
-var B = ["59", "B", "C#/Db", "D#/Eb", "E", "F#/Gb", "G#/Ab", "A#/Bb", "B" ];
-var E = ["64", "E", "F#/Gb", "G#/Ab", "A", "B", "C#/Db", "D#/Eb", "E" ];
+var G = ["55", "G", "A", "B", "C", "D", "E", "F#/Gb", "G"];
+var C = ["60", "C", "D", "E", "F", "G", "A", "B", "C"];
+var F = ["65", "F", "G", "A", "A#/Bb", "C", "D", "E", "F"];
+var Bb = ["58", "A#/Bb", "C", "D", "D#/Eb", "F", "G", "A", "A#/Bb"];
+var Eb = ["63", "D#/Eb", "F", "G", "G#/Ab", "A#/Bb", "C", "D", "D#/Eb"];
+var Ab = ["56", "G#/Ab", "A#/Bb", "C", "C#/Db", "D#/Eb", "F", "G", "G#/Ab"];
+var Db = ["61", "C#/Db", "D#/Eb", "F", "F#/Gb", "G#/Ab", "A#/Bb", "C", "C#/Db"];
+var Gb = ["66", "F#/Gb", "G#/Ab", "A#/Bb", "B", "C#/Db", "D#/Eb", "F", "F#/Gb"];
+var B = ["59", "B", "C#/Db", "D#/Eb", "E", "F#/Gb", "G#/Ab", "A#/Bb", "B"];
+var E = ["64", "E", "F#/Gb", "G#/Ab", "A", "B", "C#/Db", "D#/Eb", "E"];
 
-var allKeys  = {A: A, D: D, G: G, C: C, F: F, Bb: Bb, Eb: Eb, Ab: Ab, Db: Db, Gb: Gb, B: B, E: E};
+var allKeys = {
+    A: A,
+    D: D,
+    G: G,
+    C: C,
+    F: F,
+    Bb: Bb,
+    Eb: Eb,
+    Ab: Ab,
+    Db: Db,
+    Gb: Gb,
+    B: B,
+    E: E
+};
 var keys = Object.keys(allKeys);
 var major_scale = [0, 2, 4, 5, 7, 9, 11, 12];
 
@@ -43,92 +56,90 @@ var score = 0;
 var numCorrect = 0;
 var numQues = 30;
 var numAnswered = 0;
-var incFactor = 100/numQues;
+var incFactor = 100 / numQues;
 
-function reset(numQues){
-	$(".answer").prop('disabled', false);
-	score = 0;
-	numCorrect = 0;
-	this.numQues = numQues;
-	numAnswered = 0;
-	incFactor = 100/numQues;
-	console.log("RESET");
-	$("#progress-bar").width("1%");
-	$("#div_results").hide();
+function reset(numQues) {
+    $(".answer").prop('disabled', false);
+    score = 0;
+    numCorrect = 0;
+    this.numQues = numQues;
+    numAnswered = 0;
+    incFactor = 100 / numQues;
+    console.log("RESET");
+    $("#progress-bar").width("1%");
+    $("#div_results").hide();
 }
 
-function randomStart(){
-	$("#p_question").text(1 + Math.floor(Math.random() * 8));
-	$("#p_key").text(keys[ 1 + Math.floor(Math.random() * keys.length)]);
-	$(".answer").prop('disabled', false);
-	console.log("RANDOM START");
+function randomStart() {
+    $("#p_question").text(1 + Math.floor(Math.random() * 8));
+    $("#p_key").text(keys[1 + Math.floor(Math.random() * keys.length)]);
+    $(".answer").prop('disabled', false);
+    console.log("RANDOM START");
 }
 
 $(document).ready(function() {
-	
-	//Display first key/number 
-	randomStart();
 
-	//Handle button click 
-	$('.answer').click(function(e) {
-		numAnswered++;	
-		if(numAnswered <= numQues){
-	    var response = $(e.target).attr('id');    
-	    var question = allKeys[$("#p_key").text()][$("#p_question").text()];
-		var curr = $("#p_question").text();
-		note = parseInt(allKeys[$("#p_key").text()][0]);
-		note += major_scale[$("#p_question").text()];
-		console.log("note: " +note);
-		console.log("question: " + question);
-		
-		MIDI.noteOn(0, note, 127, 0);
-	    MIDI.noteOff(0, note, 0.75);	
-	    //Correct
-	    if (question == response){
-			console.log("Correct");
-			numCorrect++;
-			$("#span_correctResponses").text(numCorrect);
-			$(".answer").prop('disabled', false);
-			prev = curr;
-			while (curr == prev){
-				curr = 1 + Math.floor(Math.random() * 8);
-			}
-			$("#p_question").text(curr);
+    //Display first key/number 
+    randomStart();
 
-		//Incorrect	
-	    }else{
-			console.log("Incorrect");
-			$(e.target).prop('disabled', true);
-	    }
-		//Update progress bar
-		if(score >= 100){
-			score = 100;
-		}
-		else if (score < 0){
-			score = 0;
-		}
-		else{
-		score = numCorrect/numAnswered*100;
-		}
-	    $("#progress-bar").width(score + "%");
-	    $("#span_numResponses").text(numAnswered);
-		}else{
-			console.log("Quiz done");
-		}
-	  });
-	//End quiz button
-	$('#a_endQuiz').click(function() {
-		console.log("Quiz done");
-		$("#div_results").show();
-		reset(5);
-	});
-	//Reset button
-	$('#a_reset').click(function() {
-		console.log("random start");
-		randomStart();
-		reset(5);
-	});
-		
+    //Handle button click 
+    $('.answer').click(function(e) {
+        numAnswered++;
+        if (numAnswered <= numQues) {
+            var response = $(e.target).attr('id');
+            var question = allKeys[$("#p_key").text()][$("#p_question").text()];
+            var curr = $("#p_question").text();
+            note = parseInt(allKeys[$("#p_key").text()][0]);
+            note += major_scale[$("#p_question").text()];
+            console.log("note: " + note);
+            console.log("question: " + question);
+
+            MIDI.noteOn(0, note, 127, 0);
+            MIDI.noteOff(0, note, 0.75);
+            //Correct
+            if (question == response) {
+                console.log("Correct");
+                numCorrect++;
+                $("#span_correctResponses").text(numCorrect);
+                $(".answer").prop('disabled', false);
+                prev = curr;
+                while (curr == prev) {
+                    curr = 1 + Math.floor(Math.random() * 8);
+                }
+                $("#p_question").text(curr);
+
+                //Incorrect	
+            } else {
+                console.log("Incorrect");
+                $(e.target).prop('disabled', true);
+            }
+            //Update progress bar
+            if (score >= 100) {
+                score = 100;
+            } else if (score < 0) {
+                score = 0;
+            } else {
+                score = numCorrect / numAnswered * 100;
+            }
+            $("#progress-bar").width(score + "%");
+            $("#span_numResponses").text(numAnswered);
+        } else {
+            console.log("Quiz done");
+        }
+    });
+    //End quiz button
+    $('#a_endQuiz').click(function() {
+        console.log("Quiz done");
+        $("#div_results").show();
+        reset(5);
+    });
+    //Reset button
+    $('#a_reset').click(function() {
+        console.log("random start");
+        randomStart();
+        reset(5);
+    });
+
 });
 
 //-->
