954c894bbbcb0ea331545634a6f463afdb68f377
Ryan Carter <carterrjj@outlook.com>
2015-09-11 23:10:44 +0100

    v1.2 Improved midi integration New scoring system

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,5 +1,4 @@
 <?php
-
 ?>
 
 <script type="text/javascript">
@@ -23,35 +22,39 @@
 	    }
 	});
 <!--
-var A = ["null", "A", "B", "C#/Db", "D", "E", "F#/Gb", "G#/Ab", "A"];
-var D = ["null", "D", "E", "F#/Gb", "G", "A", "B", "C#/Db", "D"];
-var G = ["null", "G", "A", "B", "C", "D", "E", "F#/Gb", "G" ];
-var C = ["null", "C", "D", "E", "F", "G", "A", "B", "C" ];
-var F = ["null", "F", "G", "A", "A#/Bb", "C", "D", "E", "F" ];
-var Bb = ["null", "A#/Bb", "C", "D", "D#/Eb", "F", "G", "A", "A#/Bb" ];
-var Eb = ["null", "D#/Eb", "F", "G", "G#/Ab", "Bb", "C", "D", "D#/Eb" ];
-var Ab = ["null", "G#/Ab", "A#/Bb", "C", "C#/Db", "D#/Eb", "F", "G", "G#/Ab"  ];
-var Db = ["null", "C#/Db", "D#/Eb", "F", "F#/Gb", "G#/Ab", "A#/Bb", "C", "C#/Db" ];
-var Gb = ["null", "F#/Gb", "G#/Ab", "A#/Bb", "B", "C#/Db", "D#/Eb", "F", "F#/Gb" ];
-var B = ["null", "B", "C#/Db", "D#/Eb", "E", "F#/Gb", "G#/Ab", "A#/Bb", "B" ];
-var E = ["null", "E", "F#/Gb", "G#/Ab", "A", "B", "C#/Db", "D#/Eb", "E" ];
+var A = ["57", "A", "B", "C#/Db", "D", "E", "F#/Gb", "G#/Ab", "A"];
+var D = ["62", "D", "E", "F#/Gb", "G", "A", "B", "C#/Db", "D"];
+var G = ["55", "G", "A", "B", "C", "D", "E", "F#/Gb", "G" ];
+var C = ["60", "C", "D", "E", "F", "G", "A", "B", "C" ];
+var F = ["65", "F", "G", "A", "A#/Bb", "C", "D", "E", "F" ];
+var Bb = ["58", "A#/Bb", "C", "D", "D#/Eb", "F", "G", "A", "A#/Bb" ];
+var Eb = ["63", "D#/Eb", "F", "G", "G#/Ab", "A#/Bb", "C", "D", "D#/Eb" ];
+var Ab = ["56", "G#/Ab", "A#/Bb", "C", "C#/Db", "D#/Eb", "F", "G", "G#/Ab"  ];
+var Db = ["61", "C#/Db", "D#/Eb", "F", "F#/Gb", "G#/Ab", "A#/Bb", "C", "C#/Db" ];
+var Gb = ["66", "F#/Gb", "G#/Ab", "A#/Bb", "B", "C#/Db", "D#/Eb", "F", "F#/Gb" ];
+var B = ["59", "B", "C#/Db", "D#/Eb", "E", "F#/Gb", "G#/Ab", "A#/Bb", "B" ];
+var E = ["64", "E", "F#/Gb", "G#/Ab", "A", "B", "C#/Db", "D#/Eb", "E" ];
 
 var allKeys  = {A: A, D: D, G: G, C: C, F: F, Bb: Bb, Eb: Eb, Ab: Ab, Db: Db, Gb: Gb, B: B, E: E};
 var keys = Object.keys(allKeys);
+var major_scale = [0, 2, 4, 5, 7, 9, 11, 12];
 
 var score = 0;
-var numQues = 12;
+var numCorrect = 0;
+var numQues = 30;
 var numAnswered = 0;
 var incFactor = 100/numQues;
 
 function reset(numQues){
 	$(".answer").prop('disabled', false);
 	score = 0;
+	numCorrect = 0;
 	this.numQues = numQues;
 	numAnswered = 0;
 	incFactor = 100/numQues;
 	console.log("RESET");
 	$("#progress-bar").width("1%");
+	$("#div_results").hide();
 }
 
 function randomStart(){
@@ -62,36 +65,53 @@ function randomStart(){
 }
 
 $(document).ready(function() {
-	console.log("NumAnswered: "+ numAnswered);
 	
 	//Display first key/number 
 	randomStart();
 
 	//Handle button click 
 	$('.answer').click(function(e) {
-		
+		numAnswered++;	
 		if(numAnswered <= numQues){
 	    var response = $(e.target).attr('id');    
 	    var question = allKeys[$("#p_key").text()][$("#p_question").text()];
-
+		var curr = $("#p_question").text();
+		note = parseInt(allKeys[$("#p_key").text()][0]);
+		note += major_scale[$("#p_question").text()];
+		console.log("note: " +note);
+		console.log("question: " + question);
+		
+		MIDI.noteOn(0, note, 127, 0);
+	    MIDI.noteOff(0, note, 0.75);	
 	    //Correct
 	    if (question == response){
 			console.log("Correct");
-			score+=incFactor;
-			if (score>=100) score = 100;
+			numCorrect++;
+			$("#span_correctResponses").text(numCorrect);
 			$(".answer").prop('disabled', false);
-			$("#p_question").text(1 + Math.floor(Math.random() * 8));
+			prev = curr;
+			while (curr == prev){
+				curr = 1 + Math.floor(Math.random() * 8);
+			}
+			$("#p_question").text(curr);
 
 		//Incorrect	
 	    }else{
 			console.log("Incorrect");
 			$(e.target).prop('disabled', true);
-			score-=incFactor;
-			if (score<0) score = 0;
 	    }
 		//Update progress bar
+		if(score >= 100){
+			score = 100;
+		}
+		else if (score < 0){
+			score = 0;
+		}
+		else{
+		score = numCorrect/numAnswered*100;
+		}
 	    $("#progress-bar").width(score + "%");
-	    numAnswered++;
+	    $("#span_numResponses").text(numAnswered);
 		}else{
 			console.log("Quiz done");
 		}
@@ -99,6 +119,7 @@ $(document).ready(function() {
 	//End quiz button
 	$('#a_endQuiz').click(function() {
 		console.log("Quiz done");
+		$("#div_results").show();
 		reset(5);
 	});
 	//Reset button
@@ -106,7 +127,6 @@ $(document).ready(function() {
 		console.log("random start");
 		randomStart();
 		reset(5);
-		
 	});
 		
 });
@@ -122,6 +142,10 @@ $(document).ready(function() {
 			role="progressbar" aria-valuenow="15" aria-valuemin="0"
 			aria-valuemax="101" style="width: 1%"></div>
 	</div>
+	<p>
+		<span id="span_correctResponses">0</span> of <span
+			id="span_numResponses">0</span> correct
+	</p>
 	<div id="div_question">
 		<p id="p_key" class="text-primary">C</p>
 		<p id="p_question" class="text-info">1</p>
@@ -147,6 +171,74 @@ $(document).ready(function() {
 	<div id="div_appFoot">
 		<a id="a_endQuiz" class="btn btn-default">End Quiz</a> <a id="a_reset"
 			class="btn btn-default">Reset</a>
+	</div>
+</div>
+<div id="div_results" class="bs-docs-section" style="display: none;">
+
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="page-header">
+				<h1 id="tables">Tables</h1>
+			</div>
+
+			<div class="bs-component">
+				<table class="table table-striped table-hover ">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Column heading</th>
+							<th>Column heading</th>
+							<th>Column heading</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>Column content</td>
+							<td>Column content</td>
+							<td>Column content</td>
+						</tr>
+						<tr>
+							<td>2</td>
+							<td>Column content</td>
+							<td>Column content</td>
+							<td>Column content</td>
+						</tr>
+						<tr class="info">
+							<td>3</td>
+							<td>Column content</td>
+							<td>Column content</td>
+							<td>Column content</td>
+						</tr>
+						<tr class="success">
+							<td>4</td>
+							<td>Column content</td>
+							<td>Column content</td>
+							<td>Column content</td>
+						</tr>
+						<tr class="danger">
+							<td>5</td>
+							<td>Column content</td>
+							<td>Column content</td>
+							<td>Column content</td>
+						</tr>
+						<tr class="warning">
+							<td>6</td>
+							<td>Column content</td>
+							<td>Column content</td>
+							<td>Column content</td>
+						</tr>
+						<tr class="active">
+							<td>7</td>
+							<td>Column content</td>
+							<td>Column content</td>
+							<td>Column content</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<!-- /example -->
+		</div>
 	</div>
 </div>
 <?php 
